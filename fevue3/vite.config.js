8d8ae212277368625e373ffc59ad02abbc079a02
wb_wanzhipeng <wb_wanzhipeng@meituan.com>
2022-08-29 11:16:17 +0800

    feat: 快速打印功能

--- fevue3/vite.config.js
+++ fevue3/vite.config.js
@@ -1,12 +1,60 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base:'./',
-  server: {
-    host: '127.0.0.1',
-    port: '3000'
+export default defineConfig(({ command, mode }) => {
+  console.log(command, mode);
+  const commonConfig = {
+    plugins: [vue()],
+    base:'./',
+    resolve: {
+      alias: {
+        views: resolve(__dirname, 'src/views'),
+        components: resolve(__dirname, 'src/components'),
+        utils: resolve(__dirname, 'src/utils'),
+        src: resolve(__dirname, 'src'),
+      },
+    },
+  }
+  if (command === 'serve') {
+    return {
+      server: {
+        port: '3000'
+      },
+      build: {
+        rollupOptions: {
+          output: {
+            chunkFileNames: 'assets/[name].js',
+            entryFileNames: 'assets/[name].js',
+            assetFileNames: 'assets/[name].[ext]',
+          },
+        }
+      },
+      ...commonConfig,
+    }
+  }
+  return {
+    build: {
+      terserOptions: {
+        compress: {
+          //生产环境时移除console
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      // 取消计算文件大小，加快打包速度
+      reportCompressedSize: false,
+      sourcemap: true,
+      // assetsDir: 'static/img',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/[name].js',
+          entryFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+        },
+      },
+    },
+    ...commonConfig,
   }
 })
--- src/extension.ts
+++ src/extension.ts
@@ -10,29 +10,63 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage('欢迎使用FETools!');
 	renderSideBar();
 	vscode.commands.registerCommand('edu-tools.handleClickItem', (url, title, isOpenBrowser) => {
-        sideBarClick(url, title, isOpenBrowser);
-    });
+		sideBarClick(url, title, isOpenBrowser);
+	});
 
 	completion(context);
 
 	context.subscriptions.push(vscode.commands.registerCommand('openWebview', async () => {
-        console.log('执行');
+		console.log('执行');
 		createWebview(context.extensionPath);
-    }));
+	}));
 
-    // context.subscriptions.push(vscode.commands.registerCommand('openCfgPanel', async () => {
-    //     // 当前可视窗口
+	// context.subscriptions.push(vscode.commands.registerCommand('openCfgPanel', async () => {
+	//     // 当前可视窗口
 	// 	const { document } = vscode.window.visibleTextEditors[0];
 	// 	// 获取可视窗口文件内容
-    //     const cfgFile = await document.getText().replace(/\s\s|\n/g, '');
-    //     createWebview(context.extensionPath, cfgFile);
-    // }));
+	//     const cfgFile = await document.getText().replace(/\s\s|\n/g, '');
+	//     createWebview(context.extensionPath, cfgFile);
+	// }));
 
-	let disposable = vscode.commands.registerCommand('fetools.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from FETools!');
+	let disposable = vscode.commands.registerCommand('fetools.helloWorld', (edit: vscode.TextEditorEdit) => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+		const { selections } = editor
+		// 选中多个代码时
+		if (selections.length > 1) {
+			return
+		}
+		console.log(editor, 'editor');
+		const chraterBefore = editor.document.getText(new vscode.Range(new vscode.Position(editor.selection.start.line, 0), new vscode.Position(editor.selection.start.line, editor.selection.end.character)));
+		const selectVal = editor.document.getText(editor.selection);
+		console.log(chraterBefore, '--');
+		console.log(selectVal, '++');
+		let space;
+		try {
+			console.log(chraterBefore.match(/\w/));
+			space = (chraterBefore.match(/\w/) as any).index;
+		} catch (error) {
+			vscode.window.showInformationMessage(JSON.stringify(error));
+		}
+		
+
+		
+		console.log(space, 'space');
+		const insertVal = `${' '.repeat(space * Number(editor.options.tabSize))}console.log(${selectVal}, '${selectVal}');\r\n`
+		editor.edit(editBuilder => {
+			editBuilder.replace(new vscode.Range(editor.selection.start.line + 1, 0, editor.selection.start.line + 1, 0), insertVal)
+		})
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('copyline', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+		// const start = new vscode.Position(position.line, 0);//当前行开始位置 
+		// const range = new vscode.Range(start, position);// 返回一个数组  [{line: '2', charater: '0'}, {line: '2', charater: '10'}]
+		// const text = editor.document.getText(range);
+		// console.log(text, '当前行文本');
+	}));
 }
 
-export function deactivate() {}
+export function deactivate() { }
